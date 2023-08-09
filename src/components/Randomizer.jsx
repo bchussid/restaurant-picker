140db8e7a382e14d69f3320a892963cbc8f771a2
Ben <benchussid@Bens-MacBook-Pro.local>
2023-08-09 15:48:43 -0400

    add/code over from old project

---
+++ src/components/Randomizer.jsx
@@ -0,0 +1,14 @@
+// const RESTAURANTS = ["Queeny's", "Thai on Main", "Viceroy", "Grub", "Krill"];
+
+// function pickRest() {
+//   return RESTAURANTS[Math.floor(Math.random() * RESTAURANTS.length)];
+// }
+
+// console.log(pickRest());
+
+/* 
+juju, krill, durham_food_hall, mother_and_sons, msushi, alpaca_chicken, saint_james, chicken_bee, blue_corn, mramen, queenys, thai_on_main, grub, viceroy, enzos, pie_pushers, gocciolina, pulcinellas
+
+
+
+*/
