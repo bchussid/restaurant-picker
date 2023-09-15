466cd656fa3f0a6438ea4eb35ae171899e95600e
Ben <benchussid@bens-mbp.lan>
2023-09-15 13:26:46 -0400

    start styling card/button

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Randomizer from './components/Randomizer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Vite + React</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)} className='rounded-md bg-red-300'>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
+      <Randomizer />
     </>
   )
 }
--- src/components/Randomizer.jsx
+++ src/components/Randomizer.jsx
@@ -1,3 +1,28 @@
+import React from 'react'
+
+function Randomizer() {
+  return (
+    <>
+      <div className='flex flex-col max w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+       <h3 className='m-2 text-2xl font-semibold tracking-tight dark:text-white'>
+        Welcome to the Random Restaurant Generator
+       </h3>
+       <p> Maybe the options go here (dropdown, etc.)</p>
+       <button className='font-mono font-semibold uppercase w-48 bg-slate-100 py-3 px-4 rounded border-2 border-black shadow-[0.25rem_0.25rem_#121212] transition-transform duration-[50ms,box-shadow] active:translate-x-0.5 active:translate-y-0.5 active:shadow-[0.125rem_0.125rem_#121212]'>
+        Retro Button
+       </button>
+      </div>
+    </>
+  )
+}
+
+export default Randomizer
+
+
+
+
+
+
 // const RESTAURANTS = ["Queeny's", "Thai on Main", "Viceroy", "Grub", "Krill"];
 
 // function pickRest() {
