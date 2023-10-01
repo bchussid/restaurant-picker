53ee4e761f815794fc929694342177924222b7bb
Ben <benchussid@bens-mbp.lan>
2023-10-01 18:53:58 -0400

    add/more styles

--- src/components/Randomizer.jsx
+++ src/components/Randomizer.jsx
@@ -10,8 +10,7 @@ function Randomizer() {
     setChoice(pickedRest);
     return choice;
   }
-  // * something needs tweaking with the transition-transform duration-[...]
-  // * works better with duration-[50ms] vs how it is now
+
   // bg-gradient-to-r from-teal-400 to-yellow-200
   // bg-gradient-to-br from-teal-400 to-yellow-200
   // bg-gradient-to-r from-teal-200 to-teal-500
@@ -19,7 +18,7 @@ function Randomizer() {
 
   return (
     <>
-      <div className="flex flex-col items-center mx-auto my-4 p-6 w-3/4 bg-gradient-to-br from-emerald-400 to-cyan-400 opacity-90 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <section className="flex flex-col items-center mx-auto my-4 p-6 w-3/5 h-min bg-gradient-to-br from-emerald-400 to-cyan-400 opacity-90 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <h3 className="mb-3 text-center text-2xl font-semibold tracking-tight dark:text-white">
           Welcome to the Random Restaurant Generator
         </h3>
@@ -29,12 +28,15 @@ function Randomizer() {
         ))} */}
         <button
           onClick={pickRest}
-          className="font-mono font-semibold uppercase w-48 bg-slate-100 py-3 px-4 rounded border-2 border-black shadow-[0.25rem_0.25rem_#121212] transition-transform duration-[50ms,box-shadow] active:translate-x-0.5 active:translate-y-0.5 active:shadow-[0.125rem_0.125rem_#121212]"
+          className="font-mono font-semibold bg-gradient-to-r from-pink-400 to-rose-400 opacity-90 uppercase w-48 bg-slate-100 py-3 px-4 rounded border-2 border-black shadow-[0.25rem_0.25rem_#121212] transition-transform-box-shadow duration-[50ms] active:translate-x-0.5 active:translate-y-0.5 active:shadow-[0.125rem_0.125rem_#121212]"
         >
           Retro Button!
         </button>
-        <div className="pt-3">I choose: {choice}</div>
-      </div>
+        <span className="py-3 underline">I choose: </span>
+        <span className=" border-2 border-gray-700 rounded-md p-1 shadow-md">
+          {choice && choice}
+        </span>
+      </section>
     </>
   );
 }
