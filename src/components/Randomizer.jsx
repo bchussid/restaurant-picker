329f5697aa3b7ca2f65253b9fc9eefa307402f50
Ben <benchussid@bens-mbp.lan>
2023-10-03 21:37:07 -0400

    add/dropdown with api categories

--- src/components/Randomizer.jsx
+++ src/components/Randomizer.jsx
@@ -6,32 +6,29 @@ function Randomizer() {
   const PLACES = ["Queeny's", "Thai on Main", "Viceroy", "Grub", "Krill"];
   // const [places, setPlaces] = useState("")
   const [choice, setChoice] = useState("");
+  // categories
+  const [options, setOptions] = useState("")
+
 
   useEffect(() => {
     async function getPlaces() {
       try {
-      const res = await axios.get('/public/assets/places.json')
-      // let bars = res.bars
-      console.log(res.data)
+        const res = await axios.get("/public/assets/places.json");
+        setOptions(Object.keys(res.data))
+        return options
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
-      // setPlaces(res)
-      // console.log(places)
     }
-    getPlaces()
-  }, [])
+    getPlaces();
+  }, []);
+  
 
   function pickRest() {
     let pickedRest = PLACES[Math.floor(Math.random() * PLACES.length)];
     setChoice(pickedRest);
     return choice;
   }
-
-  // bg-gradient-to-r from-teal-400 to-yellow-200
-  // bg-gradient-to-br from-teal-400 to-yellow-200
-  // bg-gradient-to-r from-teal-200 to-teal-500
-  // bg-gradient-to-br from-emerald-400 to-cyan-400
 
   return (
     <>
@@ -40,19 +37,24 @@ function Randomizer() {
           Welcome to the Random Restaurant Generator
         </h3>
         <p className="pb-3"> Maybe the options go here (dropdown, etc.) </p>
-        {/* {PLACES.map(place => (
-          <span key={place} className="pb-2">{place}</span>
-        ))} */}
+        <label htmlFor="choices"></label>
+        <select id="choices" name="choices"
+       
+        >
+
+        {options && options.map((spots) => (
+          <option key={spots} value={spots}>
+            {spots}
+          </option>
+        ))}
+        </select>
+
         <button
           onClick={pickRest}
-          className="font-mono font-semibold bg-gradient-to-r from-pink-400 to-rose-400 opacity-90 uppercase w-48 bg-slate-100 py-3 px-4 rounded border-2 border-black shadow-[0.25rem_0.25rem_#121212] transition-transform-box-shadow duration-[50ms] active:translate-x-0.5 active:translate-y-0.5 active:shadow-[0.125rem_0.125rem_#121212]"
+          className="font-mono font-semibold bg-gradient-to-r from-pink-400 to-rose-400 opacity-90 uppercase w-48 bg-slate-100 mt-3 py-3 px-4 rounded border-2 border-black shadow-[0.25rem_0.25rem_#121212] transition-transform-box-shadow duration-[50ms] active:translate-x-0.5 active:translate-y-0.5 active:shadow-[0.125rem_0.125rem_#121212]"
         >
           Pick Spot!
         </button>
-        {/* <span className="pt-3 underline">I choose: </span> */}
-        {/* <span className=" border-2 border-gray-700 rounded-md p-1 shadow-md">
-          {choice && choice}
-        </span> */}
       </section>
       <div className="flex justify-center p-4 border-2 w-1/5 mx-auto border-gray-500 rounded-md shadow-md">
         {choice && choice}
