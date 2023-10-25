7e8585f5127fa0dfc8c15f8505c11cc9d2f43abd
Ben <71155331+bchussid@users.noreply.github.com>
2023-10-25 19:28:46 -0400

    add/start of dark mode styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import './App.css'
-import Randomizer from './components/Randomizer'
+import "./App.css";
+import Randomizer from "./components/Randomizer";
+import { MoonIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 function App() {
-
+  const [darkMode, setDarkMode] = useState(false)
   return (
     <>
+    <div className={darkMode ? 'dark' : ''}>
+      <main className="dark:bg-slate-800">
+
+      <MoonIcon className="h-10 w-10 dark:text-slate-100" onClick={() => setDarkMode(!darkMode)} />
       <Randomizer />
+      </main>
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
