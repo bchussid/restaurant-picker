0274880b35bc877b42b1262bad370f091ef10974
Ben <71155331+bchussid@users.noreply.github.com>
2024-01-12 18:31:52 -0500

    work on theme switching - still not right

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class",
+  // theme: {
+  //   extend: {},
+  // },
+  // darkMode: "class",
   plugins: ["@headlessui/tailwindcss", require('daisyui')],
   daisyui: {
     themes: ["light", "dark", "synthwave"]
