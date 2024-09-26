07b3153c0245ba9d1c79ac5f2c617182ec5ac622
Ben <71155331+bchussid@users.noreply.github.com>
2024-09-26 18:04:35 -0400

    project & styling reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
-import "./App.css";
+import "./styles/App.css";
 import Randomizer from "./components/Randomizer";
 import { MoonIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
 import ThemeSwitcher from "./components/ThemeSwitcher";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  //todo: only toggles if in lightmode is user setting, does nothing if user setting is darkmode
-  // {darkMode ? "dark" : ""}
   return (
     <>
-      <main className="min-h-screen">
-        <div className="dark:bg-slate-800">
-          {/* <MoonIcon
-            className="h-10 w-10 dark:text-slate-100"
-            onClick={() => setDarkMode(!darkMode)}
-          /> */}
-          <ThemeSwitcher />
-          <Randomizer />
-        </div>
-      </main>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Randomizer />} />
+        </Routes>
+      </Router>
     </>
   );
 }
